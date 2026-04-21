0916e32674f9dd91085e4937179ccd550e0c67cb
theanabioz <progear2008@gmail.com>
2026-04-21 23:54:45 +0100

    Refine soft screen transitions

--- frontend/src/screens/SellerHomeScreen.tsx
+++ frontend/src/screens/SellerHomeScreen.tsx
@@ -2762,7 +2762,7 @@ export function SellerHomeScreen({ currentPanel, onSwitchPanel }: SellerHomeScre
         : bottomDockReservedSpace}
     >
       <Container maxW="container.sm" px={0}>
-        <VStack key={sellerMotionKey} spacing={5} align="stretch" className="soft-screen-transition">
+        <VStack spacing={5} align="stretch">
           <VStack spacing={6} align="stretch" pt={showFullscreenHeaderContext ? 4 : 2} mb={2}>
             <VStack align="stretch" spacing={showFullscreenHeaderContext ? 3 : 0} px={1}>
               {showFullscreenHeaderContext ? (
@@ -2870,9 +2870,12 @@ export function SellerHomeScreen({ currentPanel, onSwitchPanel }: SellerHomeScre
           ) : null}
         </VStack>
 
-          {renderActiveTab()}
-
-          <SellerMiniGameCard t={t} />
+          <Box key={sellerMotionKey} className="soft-screen-transition">
+            <VStack spacing={5} align="stretch">
+              {renderActiveTab()}
+              <SellerMiniGameCard t={t} />
+            </VStack>
+          </Box>
         </VStack>
       </Container>
 
